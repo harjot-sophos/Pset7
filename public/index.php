15c0c433d1c0ab1cd5d998383b59b8916cdcc336
Harjot Kaur <harjotparmar96@yahoo.com>
2017-01-09 18:10:50 +0000

    index done

--- public/index.php
+++ public/index.php
@@ -9,5 +9,31 @@
  $id = $_SESSION["id"];
  
     // get user's portfolio
-    $rows =	query("SELECT * FROM portfolio WHERE id = ?", $id );
-?>
+    $rows =	query("SELECT * FROM portfolios WHERE id = ?", $id );
+   
+    // create new array to store all info for portfolio table
+	$portfolios = [];
+
+	// for each of user's stocks
+	foreach ($rows as $row)	
+	{   $stock = lookup($row["symbol"]);
+	   if ($stock !== false)
+	   {
+	         	  $portfolios[] = [
+		         "symbol" => $row["symbol"],
+		         "name" => $stock["name"],
+		         "shares" => $row["shares"],
+		         "total"=> $row["shares"]*$stock["price"]
+		         
+		  ];
+	   }
+	}
+	
+	// Balance left with user 
+	$cash = query("SELECT username, cash FROM users WHERE id = $id");
+    // render portfolio
+       render("portfolio.php", ["title" => "Portfolios", "portfolios" => $portfolios, "users" => $users]);
+	
+	
+	
+?>    
